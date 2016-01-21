42d21726f96662fdcb518a45559f7b3e44d448d2
Sviatoslav Bulbakha <mail@ssbb.me>
2016-01-21 16:33:32 +0500

    fix version

--- pyenv-mode-auto-pkg.el
+++ pyenv-mode-auto-pkg.el
@@ -1,6 +1,4 @@
 (define-package "pyenv-mode-auto" "0.1.0" "Automatically activates pyenv version if .python-version file exists."
   '((pyenv-mode nil)
     (s nil)
-    (f nil)
-    (bubbles "2.7.2")
-    (gnus "1.0")))
+    (f nil)))
